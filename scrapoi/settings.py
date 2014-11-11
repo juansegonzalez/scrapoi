792110fcab2168441c9c00824b8b31bc3792d236
JuanSe González <juanse.gonzalez@gmail.com>
2014-11-11 10:55:54 +0100

    scrapy project created

---
+++ scrapoi/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapoi project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapoi'
+
+SPIDER_MODULES = ['scrapoi.spiders']
+NEWSPIDER_MODULE = 'scrapoi.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapoi (+http://www.yourdomain.com)'
