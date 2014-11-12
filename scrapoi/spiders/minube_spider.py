f15cc226591b0f4744e6ef9f563ffd4c034898d8
JuanSe González <juanse.gonzalez@gmail.com>
2014-11-12 12:41:50 +0100

    Python doctrings added

--- scrapoi/spiders/minube_spider.py
+++ scrapoi/spiders/minube_spider.py
@@ -7,12 +7,20 @@ class MinubeSpider(scrapy.Spider):
     allowed_domains = ["minube.co.uk"]
 
     def __init__(self, country='spain', *args, **kwargs):
+        """
+        In the init method we retrieve the country parameter and initialize the html poi list
+        as an empty string.
+        """
         super(MinubeSpider, self).__init__(*args, **kwargs)
         self.start_urls = ['http://www.minube.co.uk/what-to-see/%s' % country]
         self.poi_list = ''
         self.country_name = country
 
     def parse(self, response):
+        """
+        This method parses two kind of data from MiNube website: names and descriptions
+        from points of interest in the website.
+        """
         for sel in response.xpath("//li[contains(@class, 'poi_cont_desc')]"):
             item = PoiItem()
             item['name'] = sel.xpath('div/span/div/a/@title').extract()[0].encode('utf-8')
@@ -21,6 +29,10 @@ class MinubeSpider(scrapy.Spider):
             yield item
 
     def closed(self, reason):
+        """
+        This method is called when the crawling process is finished and it generates
+        a html file based in a template with the parsed data.
+        """
         if reason == 'finished':
             with open ("index_template.html", "r") as t:
                 s = Template(t.read().replace('\n', ''))
